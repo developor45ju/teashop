9d9bfe04549788b0dd16524b1abcbff54d4a2845
Julien Heng <julienheng0@gmail.com>
2024-03-07 20:20:33 +0100

    Add compnents Header and finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Header from './components/Header.jsx';
+import logo from '/public/favicon.png';
 
 function App() {
+    const links = ['Home', 'Shop', 'About', 'Blog', 'Contact'];
+
+    return (
+        <Header logo={logo} links={links} />
+    )
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBell, faUser } from '@fortawesome/free-solid-svg-icons'
+
+function Header(props) {
+    const { links, logo } = props;
+    console.log(links);    
+        
+    return (
+        <>
+            <header>
+                <div className="logo"><img src={logo} alt="Tasse de thé" /></div>
+                <nav className="navbar">
+                    <ul className="navbar__items">
+                        {links.map((link, i) => (
+                            <li key={i}>
+                                <a href={`./pages/${link.toLowerCase()}.html`}>{link}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <div className="notifications">
+                    <FontAwesomeIcon icon={faBell} />
+                </div>
+                <div className="account">
+                    <FontAwesomeIcon icon={faUser} />
+                </div>
+            </header>
+        </>
+    )   
+}
+
+export default Header;  
